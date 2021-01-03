3d19c9639b9ff5f4f45fd40d4df68071368d45ab
alexandrebraine <alex.braine@gmail.com>
2021-01-03 11:26:20 +0100

    Add 3 buttons

--- tichu/tichualex.js
+++ tichu/tichualex.js
@@ -1,3 +1,6 @@
+
+
+/*
 const buttondesign = document.getElementById('buttondesign')
 let elem= document.getElementById("board4players");
 
@@ -16,4 +19,25 @@ buttondesign.addEventListener('click', () => {
 
     }
 
+})
+
+*/
+
+const layoutrow = document.getElementById('buttonrow')
+const layoutclock = document.getElementById('buttonclock')
+const layoutanticlock = document.getElementById('buttonanticlock')
+let tableplayers = document.getElementById("board4players")
+
+
+
+layoutrow.addEventListener('click', () => {
+    tableplayers.style.gridTemplateAreas = '"top" "right" "bottom" "left"'
+})
+
+layoutclock.addEventListener('click', () => {
+    tableplayers.style.gridTemplateAreas = '". top top top ." "left left . right right" ".  bottom bottom bottom ."'
+})
+
+layoutanticlock.addEventListener('click', () => {
+    tableplayers.style.gridTemplateAreas = '". top top top ." "right right . left left" ".  bottom bottom bottom ."'
 })
