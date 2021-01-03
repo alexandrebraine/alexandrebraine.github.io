34e118c4f32c6fb9deccc1c891f53c4c31d8e7dc
alexandrebraine <alex.braine@gmail.com>
2021-01-03 11:53:16 +0100

    remove unused items

--- tichu/tichualex.js
+++ tichu/tichualex.js
@@ -1,43 +1,23 @@
-
-
 /*
 const buttondesign = document.getElementById('buttondesign')
 let elem= document.getElementById("board4players");
 
 buttondesign.addEventListener('click', () => {
     if (elem.style.gridTemplateAreas === '"top" "right" "bottom" "left"') {
-
         elem.style.gridTemplateAreas = '". top top top ." "left left . right right" ".  bottom bottom bottom ."';
-
         ". top top top ."
                         "left left . right right"
                         ".  bottom bottom bottom ."
-
     } else {
-        
         elem.style.gridTemplateAreas = '"top" "right" "bottom" "left"';
-
     }
-
 })
-
 */
 
 const layoutrow = document.getElementById('buttonrow')
 const layoutclock = document.getElementById('buttonclock')
 const layoutanticlock = document.getElementById('buttonanticlock')
 let tableplayers = document.getElementById("board4players")
-
-
-
-layoutrow.addEventListener('click', () => {
-    tableplayers.style.gridTemplateAreas = '"top" "right" "bottom" "left"'
-})
-
-layoutclock.addEventListener('click', () => {
-    tableplayers.style.gridTemplateAreas = '". top top top ." "left left . right right" ".  bottom bottom bottom ."'
-})
-
-layoutanticlock.addEventListener('click', () => {
-    tableplayers.style.gridTemplateAreas = '". top top top ." "right right . left left" ".  bottom bottom bottom ."'
-})
+layoutrow.addEventListener('click', () => { tableplayers.style.gridTemplateAreas = '"top" "right" "bottom" "left"' })
+layoutclock.addEventListener('click', () => { ableplayers.style.gridTemplateAreas = '". top top top ." "left left . right right" ".  bottom bottom bottom ."' })
+layoutanticlock.addEventListener('click', () => { tableplayers.style.gridTemplateAreas = '". top top top ." "right right . left left" ".  bottom bottom bottom ."' })
